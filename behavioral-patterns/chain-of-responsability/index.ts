57d73ce7a415e0ec3f475fa0d918a327b2074d66
William Lopes <w.f.l@hotmail.com>
2023-10-05 23:19:36 -0300

    to remove ; at the end of the lines

--- behavioral-patterns/chain-of-responsability/index.ts
+++ behavioral-patterns/chain-of-responsability/index.ts
@@ -1,10 +1,10 @@
 import {Budget} from "./Budget";
 import {DiscountsCalculator} from "./DiscountsCalculator";
 
-let budgetValue: number = 300;
-let budgetQty: number = 4;
+let budgetValue: number = 300
+let budgetQty: number = 4
 
-const calculatorClass: DiscountsCalculator = new DiscountsCalculator();
-const budgetClass: Budget = new Budget(budgetValue, budgetQty);
+const calculatorClass: DiscountsCalculator = new DiscountsCalculator()
+const budgetClass: Budget = new Budget(budgetValue, budgetQty)
 
-console.log("Desconto de: ", calculatorClass.calculateDiscounts(budgetClass));
+console.log("Desconto de: ", calculatorClass.calculateDiscounts(budgetClass))
--- behavioral-patterns/strategy/index.ts
+++ behavioral-patterns/strategy/index.ts
@@ -3,10 +3,10 @@ import {Icms} from "./taxes/icms";
 import {Iss} from "./taxes/iss";
 import {Budget} from "./Budget";
 
-let budgetValue: number = 150;
+let budgetValue: number = 150
 
-const calculatorClass: CalculatorTaxes = new CalculatorTaxes();
-const budgetClass: Budget = new Budget(budgetValue);
+const calculatorClass: CalculatorTaxes = new CalculatorTaxes()
+const budgetClass: Budget = new Budget(budgetValue)
 
-console.log('ICMS', calculatorClass.calculator(budgetClass, new Icms()));
-console.log("ISS", calculatorClass.calculator(budgetClass, new Iss()));
+console.log('ICMS', calculatorClass.calculator(budgetClass, new Icms()))
+console.log("ISS", calculatorClass.calculator(budgetClass, new Iss()))
